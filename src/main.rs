b6be45fafed697f002c6beceab59ceb63cb71064
John Handley <jwhandley17@gmail.com>
2025-10-27 11:58:26 +0000

    Refactor main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod adapters;
 mod cif;
 mod csa;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use crate::{
     cif::CifTimetable,
@@ -9,6 +9,7 @@ use crate::{
 };
 use chrono::{NaiveDate, NaiveTime};
 use clap::{Parser, Subcommand};
+use geojson::FeatureCollection;
 
 #[derive(Parser)]
 struct Cli {
@@ -25,6 +26,7 @@ enum Commands {
     },
     Query {
         network_path: PathBuf,
+        #[arg(allow_hyphen_values = true)]
         lat: f64,
         #[arg(allow_hyphen_values = true)]
         lon: f64,
@@ -40,22 +42,7 @@ fn main() -> anyhow::Result<()> {
         Commands::Import {
             timetable_path,
             network_path,
-        } => {
-            let now = std::time::Instant::now();
-            eprintln!("Reading timetable");
-            let timetable = CifTimetable::read(timetable_path)?;
-            eprintln!("Done in {:?}", now.elapsed());
-
-            let now = std::time::Instant::now();
-            eprintln!("Adapting to transport network");
-            let network = TransportNetwork::try_from(&timetable)?;
-            eprintln!("Done in {:?}", now.elapsed());
-
-            let now = std::time::Instant::now();
-            eprintln!("Saving network");
-            network.save(network_path)?;
-            eprintln!("Done in {:?}", now.elapsed());
-        }
+        } => import_timetable(timetable_path, network_path),
         Commands::Query {
             network_path,
             lat,
@@ -63,21 +50,52 @@ fn main() -> anyhow::Result<()> {
             date,
             time,
         } => {
-            let now = std::time::Instant::now();
-            eprintln!("Loading network");
-            let network = TransportNetwork::load(network_path)?;
-            eprintln!("Done in {:?}", now.elapsed());
-
-            let now = std::time::Instant::now();
-            eprintln!(
-                "Querying network for arrival times starting from ({lat}, {lon}) on {date} at {time}"
-            );
-            let arrival_times = network.query_lat_lon(lat, lon, date, time);
-            let geojson = to_feature_collection(&arrival_times)?;
+            let geojson = run_query(network_path, lat, lon, date, time)?;
             println!("{}", geojson.to_string());
-            eprintln!("Done in {:?}", now.elapsed());
+            Ok(())
         }
     }
+}
+
+fn import_timetable(
+    timetable_path: impl AsRef<Path>,
+    network_path: impl AsRef<Path>,
+) -> anyhow::Result<()> {
+    let now = std::time::Instant::now();
+    eprintln!("Reading timetable");
+    let timetable = CifTimetable::read(timetable_path)?;
+    eprintln!("Done in {:?}", now.elapsed());
+
+    let now = std::time::Instant::now();
+    eprintln!("Adapting to transport network");
+    let network = TransportNetwork::try_from(&timetable)?;
+    eprintln!("Done in {:?}", now.elapsed());
+
+    let now = std::time::Instant::now();
+    eprintln!("Saving network");
+    network.save(network_path)?;
+    eprintln!("Done in {:?}", now.elapsed());
 
     Ok(())
+}
+
+fn run_query(
+    network_path: impl AsRef<Path>,
+    lat: f64,
+    lon: f64,
+    date: NaiveDate,
+    time: NaiveTime,
+) -> anyhow::Result<FeatureCollection> {
+    let now = std::time::Instant::now();
+    eprintln!("Loading network");
+    let network = TransportNetwork::load(network_path)?;
+    eprintln!("Done in {:?}", now.elapsed());
+
+    let now = std::time::Instant::now();
+    eprintln!(
+        "Querying network for arrival times starting from ({lat}, {lon}) on {date} at {time}"
+    );
+    let arrival_times = network.query_lat_lon(lat, lon, date, time);
+    eprintln!("Done in {:?}", now.elapsed());
+    to_feature_collection(&arrival_times)
 }
